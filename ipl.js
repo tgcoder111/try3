5e56ebfa2abe95491dc7afae8b67c622f24fe360
tgcoder111 <103588803+tgcoder111@users.noreply.github.com>
2022-04-20 16:30:26 +0530

    Add files via upload

---
+++ ipl.js
@@ -0,0 +1,7 @@
+const ipl = document.getElementById("ipl");
+
+ipl.onclick=()=>{
+    //action 1
+ location.href="./ipl.html";
+ 
+}
---
+++ over.js
@@ -0,0 +1,7 @@
+const Over = document.getElementById("Over")
+
+
+Over.onclick=()=>{
+    //action 1
+ location.href="./team.html";
+}
